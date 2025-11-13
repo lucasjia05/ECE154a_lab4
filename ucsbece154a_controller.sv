8d2b360dcbd44c86b214a1e965c60e59a44a41b6
Conrad <156144674+ConradPoonUCSB@users.noreply.github.com>
2025-11-13 00:16:01 -0800

    Update ucsbece154a_controller.sv
    Uncommented jal and lui instruction operations on lines 53-54

--- ucsbece154a_controller.sv
+++ ucsbece154a_controller.sv
@@ -50,8 +50,8 @@ module ucsbece154a_controller (
 	instr_Rtype_op:     controls = 12'b1_xxx_0_0_00_0_10_0;   
 	instr_beq_op:       controls = 12'b0_010_0_0_xx_1_01_0;   
 	instr_ItypeALU_op:  controls = 12'b1_000_1_0_00_0_10_0;
-	//instr_jal_op:		controls = 12'b1_011_x_0_10_x_xx_1;
-	//instr_lui_op:		controls = 12'b1_100_x_0_11_0_xx_0; // Datapath needed for lui extend, ResultSrc mux needs to support 11
+	instr_jal_op:		controls = 12'b1_011_x_0_10_x_xx_1;
+	instr_lui_op:		controls = 12'b1_100_x_0_11_0_xx_0; // Datapath needed for lui extend, ResultSrc mux needs to support 11
 	default:            controls = 12'bx_xxx_x_x_xx_x_xx_x; 
 
      //      `ifdef SIM
